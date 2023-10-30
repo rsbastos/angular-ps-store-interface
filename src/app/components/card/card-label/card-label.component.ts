cd98430b0a226f77f2d8781b1528ee8c3de1747b
rsbastos <renedossantosbastos@gmail.com>
2023-10-29 21:52:38 -0300

    card-label created

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardLabelComponent implements OnInit {
 
+  @Input()
+  gameLabel:string=""
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
